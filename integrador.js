a817c717f07022ad390a3fb4f8dcafb3ecb9ba6d
Antonella Cisale <antonella.cisale@gmail.com>
2019-11-01 10:14:20 -0300

    ejercicio integrador terminado

--- integrador.js
+++ integrador.js
@@ -4,30 +4,31 @@
  *    de listas de datos de personas. Deberia
  *    guardar por cada persona, una lista de datos
  *    que incluyan el nombre completo, la edad y 
- *    la dirección de email.
+ *    la dirección de email.  (OK)
  * 1b. Al ingresar nueva persona, validar que no 
  *     exista en la lista. Si existe, tirar
  *     un error. (Usar funcion distinta para validar
- *     y llamar dentro de la funcion de guardar persona).
+ *     y llamar dentro de la funcion de guardar persona).  (OK)
  * 2. Hacer una función que me devuelva la lista
- *    completa de personas.
+ *    completa de personas.  (OK)
  * 3. Hacer una función que me permita encontrar
- *    una persona por email.
+ *    una persona por email.   (OK)
  ***************************** 4. Hacer una función que me permita encontrar 
  *    personas por nombre o parte del nombre.
  * 
  * 
- * 5. Hacer una función para borrar personas por mail.
+ * 5. Hacer una función para borrar personas por mail.  (OK)
  * 6. Hacer una funcion que permita modificar nombre
- *    buscando por mail.
+ *    buscando por mail.   (OK)
  * 7. Hacer una funcion que permita modificar edad 
- *    buscando por mail.
+ *    buscando por mail.   (OK)
  * Observaciones: Google. Mucho google.
  * 
  ************************************************/
 
 
-let listaPersonas = [];
+const listaPersonas = []; /*la necesitamos para toda la aplicacion, por eso la ponemos afuera. 
+                            va con const para no sobreescribirla*/
  /*array vacio para incluir datos de cada persona*/
 const personaCheck = (nombre) => {
     let buscaPersonas = listaPersonas.find((persona)=>{ /**dentro del array listaPersonas va a buscar cada persona 
@@ -36,6 +37,13 @@ const personaCheck = (nombre) => {
             throw "Error - ese nombre ya existe en nuestros registros";
         } ;
     });
+    /**opcion 2:
+     * for (let i=0; i<listaPersonas.length; i++){
+     *      if(listaPersonas[i][0]== nombre){ -- no es necesario poner el let de persona. 
+     *          throw "El nombre ya existe";
+     *       }
+     * }
+     */
 }
 
 const guardarPersona = ()=>{
@@ -47,19 +55,21 @@ const guardarPersona = ()=>{
 
     persona.push(nombre,edad,mail); /*pushea a persona los 3 datos*/
     listaPersonas.push(persona); /*pushea persona a la lista general, la lista general se va armando con cada lista persona*/
-      
+    
+    /*OPCION 2: listaPersonas.push([nombre,edad,mail]) - no necesito ponerle un nombre, solo poniendolo asi esta bien*/
 }
  guardarPersona (); /*cada vez que escribo encuesta me va a pedir estos datos y los va a pushear a la lista*/
 
- const showLista = () =>{
+ const showLista = () =>{ /**opcion 2: const get listaPersonas =()=>{ return listaPersonas;}(esto puede servir
+                            para cambiar el nombre de la constante*/
      let verLista = confirm('Queres ver la lista completa?');
         if(verLista == true){
             console.log(listaPersonas);
         }
  }
  
- /*version con for de ejercicio de busqueda, pero por cada array persona salta el error de busqueda*/
-const buscaMail = ()=>{
+
+const buscaMail = ()=>{ /**hay una opcion para unir el buscaMail con el nombreCheck - para reutilizar el codigo */
     let esta = false; /**booleano para revisar que si todo el for se ejecuta, 
                     y no se encuentra lo que busco, salte el error */
     let correoABuscar = prompt("ingresa un mail para buscarlo en nuestra lista");
@@ -86,29 +96,88 @@ const borraPersona = ()=>{
         }
     }
 }
+/**OPCION 2:
+ * const deletePersonaByEmail = (email) =>{
+ *      let index = listaPersonas.findIndex((listaPersonas)=>{ - de cada index de mi array lo va a revisar
+ *          return persona[2]== mail; - que devuelva la persona 2 si la necesito - esta parte del index se puede
+ *          sacar en una nueva funcion y usarla en cada una de las funciones que la necesitan - 
+ *      });
+ *      if (index >-1){
+ *          personas.splice(index,1)}; - a partir de este elemento elimino X elementos
+ * };
+ */
 
-const buscaNombre =()=>{
 
-
-
+/** se llama JS DOC o DOCBLOCK, sirve para entender de que va la funcion
+ * 
+ * @param {*} parteNombre 
+ * @returns array|boolean
+ * @throws Error
+ */
+const buscaNombre =(parteNombre)=>{  /*en general para buscar algo, 
+                                    en el nombre  de la variable se usa get - Ej: getNombre*/
+    for(i=0;i<listaPersonas.length; i++){
+        if(listaPersonas[i][0].indexOf(parteNombre)!= -1){ /**busca posicion del string. 
+                                                            index of devuelve siempre -1 cuando no encuentra nada  */
+            return listaPersonas[i];
+           }
+    } 
+    
 }
 
-const cambiaNombre = () =>{
-    let correoABuscar = prompt("ingresa un mail para buscarlo en nuestra lista");
+/*return false - siempre cuando el for loopea y no encuentra nada, 
+es una buea practica ponerlo abajo, como que si no funciona se retorna false*/
+
+
+const cambiaNombre = (mail, nombreMod) =>{
     for (i=0; i<listaPersonas.length; i++){
-        if(correoABuscar == listaPersonas[i][2]){
+        if(mail == listaPersonas[i][2]){
             confirm("queres cambiar el nombre?")
             if(true){
-            let nombreMod = prompt('Que nombre pongo?')
-            let nombreCambiado = listaPersonas.splice(listaPersonas.findIndex(nombre => nombreMod,1); 
-                
-                
-            
+            listaPersonas[i][0]=nombreMod;
+            i=listaPersonas.length;
             }
         }
     }
 }
 
+
+const cambiaEdad = (mail, edadMod) =>{
+    for (i=0; i<listaPersonas.length; i++){
+        if(mail == listaPersonas[i][2]){
+            confirm("queres cambiar la edad?")
+            if(true){
+            listaPersonas[i][1]=edadMod;
+            i=listaPersonas.length;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const cambiaNombre = (mail, nombre) =>{
+    for (i=0; i<listaPersonas.length; i++){
+        if(mail == listaPersonas[i][2]){
+            confirm("queres cambiar el nombre?")
+            if(true){
+            let nombreMod = prompt('Que nombre pongo?')
+            listaPersonas[i][0]=nombreMod;
+            }
+        }
+    }
+}
+cambiaNombre(, nombreMod)
 
 
 
@@ -182,46 +251,3 @@ if (buscaCorreo == undefined){
 };
 
 
-
-
-
-
-
-
-
- let general=[]
- const datos=()=>{
-   let lista=[];
-   let nombre=prompt("Nombre");
-   let edad=prompt("Edad");
-   let email=prompt("Email");
-   lista.push(nombre,edad,email);
-   general.push(lista)
- }
- datos();
- let persona = [`${nombre}, ${edad}, ${mail}`];
- for (i=0; i<listaPersonas.length; i++){
-     if(listaPersonas[i]==false){
-        listaPersonas[i].push(`${persona}`);
-    }
- }
-
- 
-
-
-/*undefined*/
- let nombre = prompt('decime tu nombre');
- let edad = prompt ('decime tu edad');
- let mail = prompt ('decime tu mail');
- let arrayGral = [];
- let persona = [];
- let union;
- const encuesta = (nombre, edad, mail)=>{
-    for (let i=0; i<arrayGral; i++){
-        persona = [`${nombre}, ${edad}, ${mail}`];
-        union = arrayGral.push(persona);
-        arrayGral[i]++;
-        return arrayGral;
-    }
- }
- encuesta (nombre, edad, mail);
