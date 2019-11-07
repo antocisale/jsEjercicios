d2681600a9354aac75176759acfc53b453c635ee
Antonella Cisale <antonella.cisale@gmail.com>
2019-11-07 09:23:41 -0300

    test unitarios finalizados

--- test-unitarios/archivo.test.js
+++ test-unitarios/archivo.test.js
@@ -87,26 +87,52 @@ test ('le paso "hola" me devuelve error', () => {
     const searcher = (tarea) =>{
         let tareaError = -1
         for(let i = 0; i < toDo.length; i++){
-                if(toDo[i][0] == tarea){
+                if(toDo[i][0].indexOf( tarea) > -1){
                     return i;
                 }
             }
             return tareaError;
         };
+
     const searchTarea = (tarea) =>{
         let index = searcher(tarea);
         if (index > -1){
             return toDo[index];
-        }
+        }else{
+        return "tarea inexistente"};
     }
+
     const editTarea = (tarea, newTarea, newDescription) =>{
         let index = searcher(tarea);
         if (index > -1){
             toDo[index][0] = newTarea;
             toDo[index][1] = newDescription;
             return toDo[index];
+        }else{
+            return "tarea inexistente"};
+    }
+
+    const deleteTarea = (tarea) =>{
+        let index = searcher(tarea);
+        if (index > -1){
+            toDo.splice(index,1);
+            return toDo;
+        }else{
+            return "tarea inexistente"};
+    }
+
+    const filterTarea = (hecho) =>{
+        let arrayFiltered = toDo.filter((todoItem) =>{
+            return todoItem[2]==hecho;
+            }
+        )
+        if(arrayFiltered != 0){
+            return arrayFiltered;
+        }else{
+            return "tarea inexistente";
         }
     }
+
 
 test ('cargo tarea "prueba", "descripcion", y "done"', () =>{
     expect(addTarea("prueba", "descripcion", false)).toContainEqual(["prueba", "descripcion", false 
@@ -115,12 +141,14 @@ test ('cargo tarea "prueba", "descripcion", y "done"', () =>{
 )
 
 test ('cargo tarea "prueba 2", "descripcion 2", y "true"', () =>{
+    addTarea("prueba", "descripcion", false)
     expect(addTarea("prueba 2", "descripcion 2", true)).toContainEqual(["prueba 2", "descripcion 2", true 
         ], )
 }
 )
 
 test ('cargo tarea "prueba 3", "descripcion 3" sin informacion de si esta hecha o no', () =>{
+    addTarea
     expect(addTarea("prueba 3", "descripcion 3")).toContainEqual(["prueba 3", "descripcion 3", false 
         ], )
 }
@@ -135,7 +163,13 @@ test ('busco tarea "prueba 3" y me devuelve el array completo', () =>{
 
 test ('busco tarea "pruebas" y me devuelve error', () =>{
     addTarea("prueba 3", "description 2", true);
-    expect(searchTarea("pruebas")).toBe(-1)
+    expect(searchTarea("pruebas")).toBe("tarea inexistente")
+}
+)
+
+test ('busco tarea "pruebas" y me devuelve error', () =>{
+    addTarea("prueba 3", "description 2", true);
+    expect(editTarea("pruebas")).toBe("tarea inexistente")
 }
 )
 
@@ -145,6 +179,61 @@ test ('busco tarea "prueba 3" y edito nombre a "pruebaChange" y descripcion a "d
         .toContainEqual("pruebaChange", "pruebaDescription", false)
 }
 )
+
+test ('busco tarea "prueba 3" y la elimino', () =>{
+    addTarea("prueba 3", "description 2", true);
+    addTarea("prueba 2", "descripcion 2", false);
+    expect(deleteTarea("prueba 3")).toEqual(expect.not.arrayContaining(["prueba 3", "description 2", true]))
+}
+)
+
+test ('busco tarea "prueba 4" para eliminar y salta error porque no la encuentra', () =>{
+    addTarea("prueba 3", "description 2", true);
+    addTarea("prueba 2", "descripcion 2", false);
+    expect(deleteTarea("prueba 4")).toBe("tarea inexistente")
+}
+)
+
+test ('busco tarea "prueba 4" y salta error porque no la encuentra', () =>{
+    addTarea("prueba 3", "description 2", true);
+    addTarea("prueba 2", "descripcion 2", false);
+    expect(deleteTarea("prueba 4")).toBe("tarea inexistente")
+}
+)
+
+test ('filtro tareas hechas', () =>{
+    addTarea("prueba 3", "description 2", true);
+    addTarea("prueba 2", "descripcion 2", false);
+    addTarea("prueba 4", "description 3", true);
+    addTarea("prueba 5", "description 4", true);
+    addTarea("prueba 6", "descripcion 5", false);
+
+    expect(filterTarea(true)).toContainEqual(["prueba 3", "description 2", true],
+        ["prueba 4", "description 3", true], ["prueba 5", "description 4", true] )
+}
+)
+
+test ('filtro tareas sin hacer', () =>{
+    addTarea("prueba 3", "description 2", true);
+    addTarea("prueba 2", "descripcion 2", false);
+    addTarea("prueba 4", "description 3", true);
+    addTarea("prueba 5", "description 4", true);
+    addTarea("prueba 6", "descripcion 5", false);
+
+    expect(filterTarea(false)).toContainEqual(["prueba 2", "descripcion 2", false],
+        ["prueba 6", "descripcion 5", false] )
+}
+)
+
+test ('filtro tareas sin hacer y no encuentra ninguna', () =>{
+    addTarea("prueba 3", "description 2", true);
+    addTarea("prueba 4", "description 3", true);
+    addTarea("prueba 5", "description 4", true);
+
+    expect(filterTarea(false)).toBe("tarea inexistente")
+}
+)
+
 beforeEach(()=>{
     toDo=[]
 })
